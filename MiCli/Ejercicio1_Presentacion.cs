e0ed4d18e6eb726dd5f2ed729e1a212fe4213f2b
Gary Pimentel <141964145+inggary@users.noreply.github.com>
2026-01-23 00:06:53 +0000

    ejercicio completado

--- MiCli/Ejercicio1_Presentacion.cs
+++ MiCli/Ejercicio1_Presentacion.cs
@@ -6,8 +6,11 @@ public class Ejercicio1_Presentacion
     public static string ObtenerPresentacion()
     {
         // TODO: Declara tres variables string: nombre, carrera y hobby
+        string nombre = "Gary";
+        string carrera = "Ingeniero Sistemas";
+        string hobby = "Videojuegos";
         // TODO: Retorna un string con las tres variables en líneas separadas
-        return "reemplaza este texto con tu presentación"; //OJO: reemplaza este texto
+        return $"{nombre}\n{carrera}\n{hobby}"; //OJO: reemplaza este texto
         throw new NotImplementedException();
     }
     
--- MiCli/Ejercicio3_ErrorIntencional.cs
+++ MiCli/Ejercicio3_ErrorIntencional.cs
@@ -6,11 +6,13 @@ public class Ejercicio3_ErrorIntencional
     public static string UsarConstanteCorrectamente()
     {
         // TODO: Declara una constante int
+        const int numero = 0;
         // TODO: Intenta cambiar su valor en la siguiente línea (esto causará un error)
+        //numero = 0;
         // TODO: Lee el error que te da Visual Studio
         // TODO: Comenta la línea que causa el error
         // TODO: Retorna un mensaje con el valor de la constante
-        return "reemplaza este texto con el valor de la constante"; //OJO: reemplaza este texto
+        return $"mi constante {numero}"; //OJO: reemplaza este texto
         throw new NotImplementedException();
     }
     
